fb82ab38130beeb37f7406595d135a402654400a
Jessica Y <desh365@hotmail.com>
2018-10-13 16:34:40 -0500

    Updates

---
+++ precache-manifest.dff660aca8ca8277654668882bba4ca8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5feed8e244113a89a83429679689144c",
+    "url": "/Personal-Website/static/media/julio-grimaldo-matte.5feed8e2.png"
+  },
+  {
+    "revision": "a057f2b45916393aacfe790f86be3e35",
+    "url": "/Personal-Website/static/media/PS1.a057f2b4.png"
+  },
+  {
+    "revision": "87f34ce215affaea4209",
+    "url": "/Personal-Website/static/js/runtime~main.87f34ce2.js"
+  },
+  {
+    "revision": "63c31df9fa60fe7343bb",
+    "url": "/Personal-Website/static/js/main.63c31df9.chunk.js"
+  },
+  {
+    "revision": "4bfdd8b5964d43fbaa3a",
+    "url": "/Personal-Website/static/js/1.4bfdd8b5.chunk.js"
+  },
+  {
+    "revision": "63c31df9fa60fe7343bb",
+    "url": "/Personal-Website/static/css/main.3e56779a.chunk.css"
+  },
+  {
+    "revision": "4bfdd8b5964d43fbaa3a",
+    "url": "/Personal-Website/static/css/1.3be9ebb1.chunk.css"
+  },
+  {
+    "revision": "0645ce64a852858e38ada962498139cb",
+    "url": "/Personal-Website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c48033f1824ef83952d76db9bcfbd9a.js"
+  "/Personal-Website/precache-manifest.dff660aca8ca8277654668882bba4ca8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Personal-Website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
